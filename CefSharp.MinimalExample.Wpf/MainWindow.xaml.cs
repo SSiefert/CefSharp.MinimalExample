7ae26f208ad49494850473002792a7a382636f5d
ssiefert <mail@simon-siefert.de>
2021-04-09 19:25:12 +0200

    Reproducing OutOfMemoryException.

--- CefSharp.MinimalExample.Wpf/MainWindow.xaml.cs
+++ CefSharp.MinimalExample.Wpf/MainWindow.xaml.cs
@@ -1,12 +1,55 @@
-﻿using System.Windows;
+﻿using CefSharp.Wpf;
+using System.Timers;
+using System.Windows;
 
 namespace CefSharp.MinimalExample.Wpf
 {
     public partial class MainWindow : Window
     {
+        private ChromiumWebBrowser browser;
+
         public MainWindow()
         {
             InitializeComponent();
+
+            var timer = new Timer(5000);
+            timer.Elapsed += (object senderCreate, ElapsedEventArgs eCreate) =>
+            {
+                Create();
+
+                var timerClear = new Timer(2500);
+                timerClear.Elapsed += (object senderClear, ElapsedEventArgs eCLear) =>
+                {
+                    Clear();
+                    timerClear.Stop();
+                };
+                timerClear.Start();
+            };
+            timer.Start();
+        }
+
+        private void Create()
+        {
+            Dispatcher.Invoke(() =>
+            {
+                browser = new ChromiumWebBrowser();
+                browser.Name = "browser";
+                browser.Address = "https://www.heise.de";
+                browser.BrowserSettings.WindowlessFrameRate = 60;
+
+                Border.Child = browser;
+            });
+        }
+
+        private void Clear()
+        {
+            Dispatcher.Invoke(() =>
+            {
+                Border.Child = null;
+
+                browser.Dispose();
+                browser = null;
+            });
         }
     }
 }
